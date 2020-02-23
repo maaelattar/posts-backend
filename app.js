53cc539ed431f7233fe8bb9fc3357f785dd277a0
MaaElattar <maaelattar@gmail.com>
2020-02-23 22:15:28 +0200

    Update app static routes

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(
 	})
 );
 app.use(express.static(path.join(__dirname, 'public', 'posts-app')));
+app.use('*', express.static(path.join(__dirname, 'public', 'posts-app')));
 app.use(morgan('dev'));
 app.use(cors());
 
