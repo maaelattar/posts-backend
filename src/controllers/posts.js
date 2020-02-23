982ee818f2998c09ecd140feed4dbacc4128090d
MaaElattar <maaelattar@gmail.com>
2020-02-23 20:23:25 +0200

    Refactor controllers

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,126 +1,124 @@
-const Post = require("../models/post");
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
 
-exports.createPost = (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: url + "/images/" + req.file.filename,
-        creator: req.userData.userId
-    });
-    post
-        .save()
-        .then(createdPost => {
-            res.status(201).json({
-                message: "Post added successfully",
-                post: {
-                    ...createdPost,
-                    id: createdPost._id
-                }
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "Creating a post failed!"
-            });
-        });
+const populatePost = (req, res, next, id) => {
+	Post.findOne({ _id: id })
+		.then(post => {
+			if (!post) {
+				return res.sendStatus(404);
+			}
+			req.post = post;
+			return next();
+		})
+		.catch(next);
+};
+const createPost = (req, res, next) => {
+	const post = new Post({
+		title: req.body.title,
+		content: req.body.content,
+		imagePath: req.file.url
+	});
+	post.creator = req.userData.userId;
+	post
+		.save()
+		.then(createdPost => {
+			res.status(201).json({
+				post: createdPost.toJSON()
+			});
+		})
+		.catch(next);
 };
 
-exports.updatePost = (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-        const url = req.protocol + "://" + req.get("host");
-        imagePath = url + "/images/" + req.file.filename;
-    }
-    const post = new Post({
-        _id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: imagePath,
-        creator: req.userData.userId
-    });
-    Post.updateOne({
-            _id: req.params.id,
-            creator: req.userData.userId
-        }, post)
-        .then(result => {
-            if (result.n > 0) {
-                res.status(200).json({
-                    message: "Update successful!"
-                });
-            } else {
-                res.status(401).json({
-                    message: "Not authorized!"
-                });
-            }
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "Couldn't udpate post!"
-            });
-        });
+const updatePost = (req, res, next) => {
+	if (req.userData.userId.toString() === req.post.creator._id.toString()) {
+		const post = req.post;
+		post.title = req.body.title;
+		post.content = req.body.content;
+		if (req.file) {
+			post.imagePath = req.file.url;
+		} else {
+			post.imagePath = req.body.imagePath;
+		}
+		Post.updateOne(
+			{
+				_id: req.params.id,
+				creator: req.userData.userId
+			},
+			post
+		)
+			.then(result => {
+				if (result.n > 0) {
+					res.status(200).json({ message: 'Post updated successfully' });
+				} else {
+					res.status(400).json({ message: 'Not authorized' });
+				}
+			})
+			.catch(next);
+	} else {
+		return res.sendStatus(403);
+	}
 };
 
-exports.getPosts = (req, res, next) => {
-    const pageSize = +req.query.pageSize
-    const currentPage = +req.query.page
+const getPosts = (req, res, next) => {
+	const postsPerPage = +req.query.postsPerPage;
+	const currentPage = +req.query.currentPage;
 
-    const postQuery = Post.find()
-    let fetchedPosts;
+	const postQuery = Post.find();
+	let fetchedPosts;
 
-    if (pageSize && currentPage) {
-        postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize)
-    }
-    postQuery.then(documents => {
-        fetchedPosts = documents
-        return Post.count()
-    }).then(count => {
-        res.status(200).json({
-            message: 'Posts fetched successfully',
-            posts: fetchedPosts,
-            maxPosts: count
-        })
-    }).catch(error => {
-        res.status(500).json({
-            message: 'Fetching posts failed'
-        })
-    })
-}
+	if (postsPerPage && currentPage) {
+		postQuery.skip(postsPerPage * (currentPage - 1)).limit(postsPerPage);
+	}
+	postQuery
+		.then(documents => {
+			fetchedPosts = documents.map(post => {
+				return post.toJSON();
+			});
+			return Post.countDocuments();
+		})
+		.then(count => {
+			res.status(200).json({
+				posts: fetchedPosts,
+				postsCount: count
+			});
+		})
+		.catch(next);
+};
 
-exports.getPost = (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post)
-        } else {
-            res.status(404).json({
-                message: 'Post not found'
-            })
-        }
-    }).catch(error => {
-        res.status(500).json({
-            message: 'Fetching post failed'
-        })
-    })
-}
+const getPost = (req, res, next) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			if (post) {
+				res.status(200).json(post.toJSON());
+			} else {
+				res.status(404).json({
+					message: 'Post not found'
+				});
+			}
+		})
+		.catch(next);
+};
 
-exports.deletePost = (req, res, next) => {
-    Post.deleteOne({
-            _id: req.params.id,
-            creator: req.userData.userId
-        }).then(result => {
-            if (result.n > 0) {
-                res.status(200).json({
-                    message: 'Post deleted successfully'
-                })
-            } else {
-                res.status(401).json({
-                    message: 'Not authorized'
-                })
-            }
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: 'Deleting post failed'
-            })
-        })
-}
+const deletePost = (req, res, next) => {
+	if (req.userData.userId.toString() === req.post.creator._id.toString()) {
+		Post.deleteOne({
+			_id: req.params.id,
+			creator: req.userData.userId
+		})
+			.then(result => {
+				return res.sendStatus(204);
+			})
+			.catch(next);
+	} else {
+		return res.sendStatus(403);
+	}
+};
+
+module.exports = {
+	populatePost,
+	createPost,
+	updatePost,
+	getPost,
+	getPosts,
+	deletePost
+};
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,61 +1,62 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
-const User = require('../models/user');
+const createUser = (req, res, next) => {
+	if (!req.body.user.email) {
+		return res.status(422).json({ errors: { email: "can't be blank" } });
+	}
+	if (!req.body.user.password) {
+		return res.status(422).json({ errors: { password: "can't be blank" } });
+	}
+	let user = new User();
+	user.email = req.body.user.email;
+	user.setPassword(req.body.user.password);
+	user
+		.save()
+		.then(user => {
+			res.status(201).json({
+				user: user.toJSON()
+			});
+		})
+		.catch(next);
+};
+const getUser = (req, res, next) => {
+	User.findById(req.userData.userId)
+		.then(user => {
+			if (!user) {
+				return res.sendStatus(401);
+			} else {
+				return res.status(200).json({ user: user.toJSON() });
+			}
+		})
+		.catch(next);
+};
+const userLogin = (req, res, next) => {
+	if (!req.body.user.email) {
+		return res.status(422).json({ errors: { email: "can't be blank" } });
+	}
 
-exports.createUser = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        })
-        user.save().then(result => {
-            res.status(201).json({
-                message: 'User created',
-                result: result
-            })
-        }).catch(error => {
-            res.status(500).json({
-                message: 'Invalid authentication credentials'
-            })
-        })
-    })
-}
+	if (!req.body.user.password) {
+		return res.status(422).json({ errors: { password: "can't be blank" } });
+	}
+	User.findOne({
+		email: req.body.user.email
+	})
+		.then(user => {
+			if (!user || !user.validatePassword(req.body.user.password)) {
+				return res.status(401).json({
+					errors: { 'email or password': 'is invalid' }
+				});
+			} else {
+				user.token = user.generateJWT();
+				return res.status(200).json({ user: user.toJSON() });
+			}
+		})
+		.catch(next);
+};
 
-exports.userLogin = (req, res, next) => {
-    let fetchedUser;
-    User.findOne({
-        email: req.body.email
-    }).then(user => {
-        if (!user) {
-            return res.status(401).json({
-                message: 'Auth failed'
-            })
-        }
-        fetchedUser = user
-        return bcrypt.compare(req.body.password, user.password)
-    }).then(result => {
-        if (!result) {
-            return res.status(401).json({
-                message: 'Auth failed'
-            })
-        }
-        const token = jwt.sign({
-                email: fetchedUser.email,
-                userId: fetchedUser._id
-            },
-            process.env.JWT_KEY, {
-                expiresIn: '3 days'
-            }
-        )
-        return res.status(200).json({
-            token: token,
-            expiresIn: '3 days',
-            userId: fetchedUser._id
-        })
-    }).catch(error => {
-        return res.status(401).json({
-            message: 'Invalid authentication credentials'
-        })
-    })
-}
+module.exports = {
+	getUser,
+	createUser,
+	userLogin
+};
