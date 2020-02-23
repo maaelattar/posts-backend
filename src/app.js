197d52364b641536bfe43bfb52974da20cad6982
MaaElattar <maaelattar@gmail.com>
2020-02-23 20:25:36 +0200

    Refactor app

--- src/app.js
+++ src/app.js
@@ -1,45 +1,60 @@
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('book-code:server');
-const path = require('path')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const postsRoutes = require('./routes/posts')
-const userRoutes = require('./routes/user')
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+
+const chalk = require('chalk');
+
+require('./models/User');
+require('./models/Post');
 
 const app = express();
+const port = process.env.PORT;
+
+let isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+	app.use(errorhandler());
+	mongoose.set('debug', true);
+
+	global.chalk = chalk;
+	global.log = console.log;
+}
 
 const mongoUrl = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.PW}@cluster0-9ptgq.mongodb.net/mean-posts-app?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to database')
-}).catch(() => {
-    console.log('Connection failed')
-})
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+mongoose
+	.connect(mongoUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('Connected to database');
+	})
+	.catch(() => {
+		console.log('Connection failed');
+	});
+
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
+app.use('/images', express.static(path.join('images')));
 app.use(morgan('dev'));
+app.use(cors());
 
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
+app.use('/api/posts', postsRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(port, () => {
+	console.log('Server is running on port:', port);
 });
-
-app.use('/api/posts', postsRoutes)
-app.use('/api/user', userRoutes)
-
-module.exports = app;
